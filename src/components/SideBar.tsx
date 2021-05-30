9a1ba3beddd04ae19c56a0d549fdbbe2e3c6f79e
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@ambevtech.com.br>
2021-05-30 18:02:52 -0300

    SideBar linha 19 modificado, declaração de constantes e função para um só tipo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,40 +4,40 @@ import { Button } from './Button';
 
 import '../styles/sidebar.scss';
 
-//duplicando interface do outro componente
-//Pesquisar se há como aproveitar esta interface sem precisar declarala novamente
 interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
 }
 
-interface SideBarProps {
-  //importando e tipando const do outro componente
-  genres: GenreResponseProps[];
-  selectedGenreId: number;
-  //importando e tipando a função de outro componente
-  handleClickButton: (id: number) => void;
+interface SideBarProps{
+genres : GenreResponseProps [];
+selectedGenreId : number;
+handleClickButton : (id : number ) => void;
 }
-//({const, const, função : componente proveniente})
-export  function SideBar ({genres, selectedGenreId, handleClickButton}: SideBarProps) {
-  return (
-    <nav className="sidebar">
-    <span>Watch<p>Me</p></span>
 
-    <div className="buttons-container">
-      {genres.map(genre => (
-        <Button
-          key={String(genre.id)}
-          title={genre.title}
-          iconName={genre.name}
-          onClick={() => handleClickButton(genre.id)}
-          selected={selectedGenreId === genre.id}
-        />
-      ))}
-    </div>
+export function SideBar( props : SideBarProps) {
 
-  </nav>
+  const {genres,selectedGenreId,handleClickButton} = props;
+  
 
-  )
+return(
+  <nav className="sidebar">
+  <span>Watch<p>Me</p></span>
+
+  <div className="buttons-container">
+    {genres.map(genre => (
+      <Button
+        key={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => handleClickButton(genre.id)}
+        selected={selectedGenreId === genre.id}
+      />
+    ))}
+  </div>
+
+</nav>
+)
+
 }
